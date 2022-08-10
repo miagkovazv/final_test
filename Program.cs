f1fdd02683462ae799f9d23afa0a3abc1c9939ab
miagkovazv <108032578+miagkovazv@users.noreply.github.com>
2022-08-10 20:25:00 +0300

    добавлен код программы

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите массив строк: ");
+string [] array = new string [5];
+for (int i = 0; i < array.Length; i++)
+{
+    array [i] = Console.ReadLine();
+}
+Console.WriteLine("Преобразованный массив: ");
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    Console.Write(array[i] + " ");
+}
